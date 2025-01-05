f578daba071fc61dd63604ff3c1e2bf89172ddd3
rajalamdev <rajalamofficial@gmail.com>
2025-01-06 06:58:55 +0700

    deploy

--- app/layout.tsx
+++ app/layout.tsx
@@ -4,19 +4,10 @@ import "./globals.css";
 import Navbar from "@/components/Navbar";
 import Footer from "@/components/Footer";
 
-const geistSans = Geist({
-  variable: "--font-geist-sans",
-  subsets: ["latin"],
-});
-
-const geistMono = Geist_Mono({
-  variable: "--font-geist-mono",
-  subsets: ["latin"],
-});
-
 const poppins = Poppins({
   variable: "--font-poppins",
   weight: ["100", "200", "300", "400", "500", "600", "700", "800", "900"],
+  subsets: ["latin"]
 });
 
 
@@ -33,7 +24,7 @@ export default function RootLayout({
   return (
     <html lang="en">
       <body
-        className={`${geistSans.variable} ${geistMono.variable} ${poppins.variable} antialiased`}
+        className={` ${poppins.variable} antialiased`}
       >
         <header>
           <Navbar />
