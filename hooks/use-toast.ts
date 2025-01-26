d1978e6b07bd22cb0f89572ff71042a960d92a19
rajalamdev <rajalamofficial@gmail.com>
2025-01-26 19:25:01 +0700

    fix: add any in toast type lol

--- hooks/use-toast.ts
+++ hooks/use-toast.ts
@@ -13,7 +13,7 @@ const TOAST_REMOVE_DELAY = 1000000
 
 type ToasterToast = ToastProps & {
   id: string
-  title?: React.ReactNode
+  title?: React.ReactNode | any
   description?: React.ReactNode
   action?: ToastActionElement
 }
