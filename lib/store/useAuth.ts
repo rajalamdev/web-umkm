64d9bf76dfb71e5c9443b9f3059de485db26f4bb
rajalamdev <rajalamofficial@gmail.com>
2025-01-29 05:52:41 +0700

    style: remove gradient background in home page

--- lib/store/useAuth.ts
+++ lib/store/useAuth.ts
@@ -9,25 +9,24 @@ interface User {
   phone: string | null;
   photo_url: string | null;
   username: string | null;
-  // Tambahkan properti lain yang Anda butuhkan
 }
 
 interface AuthStore {
   token: string | null;
   user: User | null; // Simpan semua informasi pengguna
   setToken: (token: string) => void;
-  setUser: (user: User) => void; // Fungsi untuk mengatur informasi pengguna
+  setUser: (user: User) => void; 
   clearAuth: () => void; // Hapus token dan informasi pengguna
 }
 
 export const useAuth = create<AuthStore>((set) => ({
   token: typeof window !== 'undefined' ? localStorage.getItem("token") : null, // Ambil token dari localStorage saat inisialisasi jika di browser
-  user: null, // Inisialisasi dengan null
+  user: null,
   setToken: (token) => {
     localStorage.setItem("token", token); // Simpan token di localStorage
     set({ token });
   },
-  setUser: (user) => set({ user }), // Fungsi untuk mengatur informasi pengguna
+  setUser: (user) => set({ user }),
   clearAuth: () => {
     localStorage.removeItem("token"); // Hapus token dari localStorage
     set({ token: null, user: null });
